2ad3c677dd2cd3fdfec3bd6c93d3cea208bc00fa
Olena-Budnik <oobudnik@gmail.com>
2023-01-24 18:59:44 +0100

    правила роботи з JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
+const h1 = <h1 id="title">Hello React.js</h1>
+// const h1 = React.createElement('h1', { id: 'title' }, 'Hello React')
 console.log(h1)
 
-const list = React.createElement(
-  'ul',
-  null,
-  React.createElement('li', null, 'list Item 1'),
-  React.createElement('li', null, 'list Item 2'),
-  React.createElement('li', null, 'list Item 3')
-)
+// const list = React.createElement(
+//     'ul',
+//     null,
+//     React.createElement('li', null, 'list Item 1'),
+//     React.createElement('li', null, 'list Item 2'),
+//     React.createElement('li', null, 'list Item 3')
+// )
+let a = 10;
+let b= 'Hello test'
+
+const list = <ul>
+  <li>list Item {a}</li>
+  <li>list Item {b}</li>
+  <li>list Item {a+10}</li>
+</ul>
 
 const content = (
-  <div>
-  {h1}
-  {list}
-  </div>
+    <div>
+        {h1}
+        {list}
+    </div>
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
