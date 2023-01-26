74f37d5b2c4cc3b9ee608c7a55fa02d7473662a8
Olena-Budnik <oobudnik@gmail.com>
2023-01-26 19:26:47 +0100

    rename Content.module.css уникальный стиль динам название

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,14 +1,15 @@
-import './Content.css'
+import classes from './Content.module.css'
 type Props = {
     text1: string
     text2: string
     year: number
 }
+console.log(classes)
 export const Content = (props: Props) => {
     let data = false
     return (
         <>
-            <p className='Content'>
+            <p className={`${classes.text}`}>
                 {props.text1}
             </p>
             <p>{props.text2}</p>
