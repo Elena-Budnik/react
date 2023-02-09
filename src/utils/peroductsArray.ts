c57dda1660560ec92ecbbd4690fccb77af8a0781
Olena-Budnik <oobudnik@gmail.com>
2023-02-09 13:53:27 +0100

    add key = {id}

--- src/utils/peroductsArray.ts
+++ src/utils/peroductsArray.ts
@@ -1,4 +1,5 @@
 type Product = {
+    id: number
     title: string
     description: string
     type: string
@@ -7,47 +8,47 @@ type Product = {
 }
 
 const productsArray: Product[] = [
-    {
+{       id:1,
         title: "iPhone 14 Pro",
         description: "This is iPhone 14 Pro",
         type: "phone",
         capacity: "256",
         price: 1200,
 },
-{
+{   id:2,
     title: "iPhone 13 Pro",
     description: "This is iPhone 13 Pro",
     type: "phone",
     capacity: "128",
     price: 1000,
 },
-{
+{   id:3,
     title: "iPhone 12 Pro",
     description: "This is iPhone 12 Pro",
     type: "phone",
     capacity: "64",
     price: 500,
 },
-{
+{   id:4,
     title: "iPhone X",
     description: "This is iPhone X",
     type: "phone",
     capacity: "256",
     price: 1000,
 },
-{
-title: "iPhone 6",
-description: "This is iPhone 6 ",
-type: "phone",
-capacity: "128",
-price: 600,
+{       id:5,
+        title: "iPhone 6",
+        description: "This is iPhone 6 ",
+        capacity: "128",
+        type: "phone",
+        price: 600,
 },
-{
-title: "iPhone 6S ",
-description: "This is iPhone 6S ",
-type: "phone",
-capacity: "64",
-price: 700,
+{       id:6,
+        title: "iPhone 6S ",
+        description: "This is iPhone 6S ",
+        type: "phone",
+        capacity: "64",
+        price: 700,
 },
 ]
 export default productsArray
